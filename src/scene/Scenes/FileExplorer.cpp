aeea91ad1655910b9718e7d514421fdc5f1e0897
Louis Lafosse <louis.lafosse@epitech.eu>
2024-01-28 06:13:44 +0100

    feat(prout) : prouted

--- src/scene/Scenes/FileExplorer.cpp
+++ src/scene/Scenes/FileExplorer.cpp
@@ -15,12 +15,12 @@ FileExplorer::FileExplorer(sf::RenderWindow &window, std::string &path) : _windo
     std::unordered_map<std::string, std::string> folder = this->_yml.getFolderContent("Notes", false);
     _text = new Text*[folder.size()];
     int i = 0;
-    int y = 10;
-    int columnWidth = 500; // Adjust this value to change the space between columns
-    int rowHeight = 50; // Adjust this value to change the space between rows
+    int y = 200; // Increase this value to move the text down
+    int columnWidth = 500; // Decrease this value to bring the columns closer together
+    int rowHeight = 200; // Decrease this value to bring the texts closer together
     for (const auto& file : folder) {
-        int x = (i % 2) * columnWidth + 100; // Calculate x position based on index
-        _text[i] = new Text(file.first, sf::Vector2f(x, y), 10, sf::Color::Black, "assets/Aileron-SemiBold.ttf");
+        int x = (i % 2) * columnWidth + 400; // Calculate x position based on index
+        _text[i] = new Text(file.first, sf::Vector2f(x, y), 15, sf::Color::Black, "assets/Aileron-SemiBold.ttf");
         if (i % 2 == 1) {
             y += rowHeight; // Move to the next row after every second file
         }
